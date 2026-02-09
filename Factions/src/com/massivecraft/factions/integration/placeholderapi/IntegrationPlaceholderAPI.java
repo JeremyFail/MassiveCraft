868396f54aba301b9897882f73fa4e056c6a170d
JeremyFail <pianomaster88@att.net>
2026-02-08 17:40:49 -0700

    MC-131 PlaceholderAPI Improvements

--- Factions/src/com/massivecraft/factions/integration/placeholderapi/IntegrationPlaceholderAPI.java
+++ Factions/src/com/massivecraft/factions/integration/placeholderapi/IntegrationPlaceholderAPI.java
@@ -1,10 +1,6 @@
 package com.massivecraft.factions.integration.placeholderapi;
 
 import com.massivecraft.massivecore.Integration;
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class IntegrationPlaceholderAPI extends Integration
 {
@@ -35,18 +31,4 @@ public class IntegrationPlaceholderAPI extends Integration
 		if (PlaceholderFactions.get().isRegistered()) return;
 		PlaceholderFactions.get().register();
 	}
-
-	// If the PlaceholderAPI command is run to reload the config
-	// then we should reregister.
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void lookForCommand(PlayerCommandPreprocessEvent event)
-	{
-		String str = event.getMessage();
-		if (str.startsWith("/")) str = str.substring(1);
-
-		if (!str.startsWith("papi")) return;
-		Bukkit.getScheduler().runTaskLater(this.getPlugin(), IntegrationPlaceholderAPI::ensureRegistered, 10L);
-	}
-
-	
 }
